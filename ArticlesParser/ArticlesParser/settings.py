c3dd6958ede04e94aba0400483d5b9598b33f2b5
khalid moussaid <khalidmossaid@gmail.com>
2020-02-08 10:51:09 +0100

    remove password from git

--- ArticlesParser/ArticlesParser/settings.py
+++ ArticlesParser/ArticlesParser/settings.py
@@ -8,13 +8,13 @@ NEWSPIDER_MODULE = 'ArticlesParser.spiders'
 ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
-    'ArticlesParser.pipelines.ArticlesparserPipeline': 300,
+	'ArticlesParser.pipelines.ArticlesparserPipeline': 300,
 }
 
 # mongoDB data
-MONGO_USER = "kmoussai"
-MONGO_PASSWORD = "H3pthJCVOMPSgD5I"
-MONGO_SERVER = "cluster0-io2fq.mongodb.net"
+MONGO_USER = "<User>"
+MONGO_PASSWORD = "<Password>"
+MONGO_SERVER = "<Server>"
 MONGO_DB_NAME = "Challenge"
 MONGO_DB_COLLECTION = "Articles"
 
